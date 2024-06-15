b836ba302deb28efc0c944a8f3c6190473fe222f
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2024-06-15 17:20:54 +0000

    completed hero section and second main page

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// const intrOne = document.querySelectorAll(".intro-one");
+// // const intrTwo = document.querySelectorAll(".intro-two");
+// console.log("connect")
+// let observer = new IntersectionObserver(enteries => {
+
+//     enteries.forEach(entry => {
+
+//         entry.target.classList.add("fade-in", entry.isIntersecting)
+
+//         // if(entry.isIntersecting){
+//         //     observer.unobserve(entry.target)
+//         // }
+
+//     })
+
+// }, {
+//     threshold: 1
+// })
+
+// intrOne.forEach(x => {observer.observe(x)})
+// // observer.observe(intrOne)
+// // observer.observe(intrTwo)
