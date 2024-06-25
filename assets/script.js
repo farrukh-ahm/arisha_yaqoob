cd9932c90c0ec6e3f4dbe42e096c4c47873e6b2f
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2024-06-25 15:30:25 +0000

    Add animation for the poject section

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,46 @@
-// const intrOne = document.querySelectorAll(".intro-one");
-// // const intrTwo = document.querySelectorAll(".intro-two");
-// console.log("connect")
-// let observer = new IntersectionObserver(enteries => {
+// Project Info and Image observer
 
-//     enteries.forEach(entry => {
+const infos = document.querySelectorAll(".project-info")
+const imgs = document.querySelectorAll(".project-image")
 
-//         entry.target.classList.add("fade-in", entry.isIntersecting)
+const infoObserver = new IntersectionObserver(entries=>{
 
-//         // if(entry.isIntersecting){
-//         //     observer.unobserve(entry.target)
-//         // }
+    entries.forEach(entry => {
+        
+        entry.target.classList.toggle("info-appear", entry.isIntersecting)
 
-//     })
+        if(entry.isIntersecting){
+            infoObserver.unobserve(entry.target)
+        }
 
-// }, {
-//     threshold: 1
-// })
+    })
 
-// intrOne.forEach(x => {observer.observe(x)})
-// // observer.observe(intrOne)
-// // observer.observe(intrTwo)
+}, {
+    threshold: 0.5
+})
+
+infos.forEach(info => {
+    infoObserver.observe(info)
+})
+
+
+
+const imgObserver = new IntersectionObserver(entries=>{
+
+    entries.forEach(entry=>{
+
+        entry.target.classList.toggle("image-appear", entry.isIntersecting)
+
+        if(entry.isIntersecting){
+            imgObserver.unobserve(entry.target)
+        }
+
+    })
+
+},{
+    threshold: 0.5
+})
+
+imgs.forEach(image=>{
+    imgObserver.observe(image)
+})
